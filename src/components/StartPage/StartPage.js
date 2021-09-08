0c8e5cdd7163595d46a6c7423047baf2f01280f3
sue2021 <carpentersuzanne@johndeere.com>
2021-09-08 15:38:45 -0500

    add start page

--- src/components/StartPage/StartPage.js
+++ src/components/StartPage/StartPage.js
@@ -4,3 +4,44 @@ import './StartPage.css';
 import ReactDOM from 'react';
 
 
+
+
+
+class StartPage extends Component {
+    constructor() {
+        this.state = {};
+    }
+    render() {
+        return <h1>Welcome to the serch art program</h1>
+
+
+
+
+
+        const artList = (props) => {
+            let artList = props.allArt.map((art) => {
+                return <h2>{art}</h2>;
+            });
+            return (
+                <div>
+                    htmlFor="search-form"{'>'}
+                    <input
+                        type="search"
+                        name="search-form"
+                        id="search-form"
+                        className="search-input"
+                        // placeholder="Search for..." value={q}
+                        // onChange={(e) => setQ(e.target.value)}
+                    />
+                    /{'>'}
+                    <button onClick={props.getArt}>Search Art</button>
+                    {artList}
+                </div>
+            );
+        };
+    }
+
+
+}    
+export default StartPage;
+
