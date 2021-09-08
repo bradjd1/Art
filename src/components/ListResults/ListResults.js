94dc25977a6209e95546b0b59198d7895caad474
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-09-08 14:03:10 -0500

    Basic ResultList page defined

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -1,4 +1,28 @@
 import './ListResults.css';
 
+class ListResults extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            id: '',
+            imageId: '',
+            keyword: ''
+        }
+    }
+    render() {
+        console.log('in results',this.props)
+        return (
+            <div className="ListResults">
+                <nav>
+                    <Link to='/'>Back</Link>
+                </nav>
+                <div className='results'></div>
+              <div>Start page</div>
+              <div>{this.apiGetArt()}</div>
+            </div>
+        );
+    }
+  }
+  
 
 export default ListResults;
