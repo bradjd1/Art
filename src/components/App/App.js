01abaeaac38645e437bcfee7f091d32ccc36fe6f
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-09-08 11:28:16 -0500

    Create initial API call in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,34 @@
+import Axios from "axios";
+import React, { Component } from 'react';
 import './App.css';
-import StartPage from '../StartPage/StartPage';
+// import StartPage from '../StartPage/StartPage';
 
-function App() {
-  return (
-    <div className="App">
-      <StartPage />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          id: '',
+          imageId: '',
+          keyword: ''
+      }
+  }
+
+apiGetArt = async (keyword) => {
+  keyword = 'cats'
+  let results = await Axios.get("https://api.artic.edu/api/v1/artworks/search?q="+keyword)
+  console.log(results);
+  console.log('id is',results.data.data[0].id);
+}
+
+  render() {
+      return (
+          <div className="App">
+            {/* <StartPage /> */}
+            <div>Start page</div>
+            <div>{this.apiGetArt()}</div>
+          </div>
+      );
+  }
 }
 
 export default App;
