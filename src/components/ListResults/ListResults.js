1e638a35b1a44556653136e1c85645fe94449061
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-09-09 17:25:09 -0500

    Add styling to nav link and ListResults page

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -16,27 +16,31 @@ class ListResults extends Component {
             return (
                 <div className='ListContainer' key={value.id}>
                     <Link to={'/results/' + value.id}>
-                    <p> {value.title}   </p>
+                        <p> {value.title}   </p>
                     </Link>
                 </div>
             )
         })
-        console.log('in results',this.props)
+        console.log('in results', this.props)
         return (
             <div className="ListResults">
                 <nav className='nav'>
                     <Link to='/'>Home</Link>
 
                 </nav>
-                <br></br>
-                <p>Click on a title below to get details about the art and/or see a picture of it.</p>
-                <h2>Art Title:</h2>
-                <div className='results'></div>
-              <div>{artList}</div>
+                <div className='background'>
+                    <br></br>
+                    <div className='background2'>
+                        <p className='instr'>Click on a title below to get details about the art piece and see a picture of it.</p>
+                        <h2>Art Title:</h2>
+                        <div className='results'></div>
+                        <div>{artList}</div>
+                    </div>
+                </div>
             </div>
         );
     }
-  }
-  
+}
+
 
 export default ListResults;
