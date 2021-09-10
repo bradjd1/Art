dbb47ce5673bb8b4414d6b1a875150d587ef7d1c
Andy Buske <buskeandrewt@johndeere.com>
2021-09-10 10:12:13 -0500

    one cube on Details page

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -15,36 +15,53 @@ class Details extends Component {
     }
 
     componentDidMount = () => {
-        console.log('this.props: ',this.props.match.params.id)
+        console.log('this.props: ', this.props.match.params.id)
         this.apiGetDetails();
     }
 
     apiGetDetails = async () => {
         const id = this.props.match.params.id
         const fields = '?fields=artist_title,title,thumbnail,place_of_origin,date_end,classification_title,image_id,id'
-        let detailResults = await Axios.get("https://api.artic.edu/api/v1/artworks/"+id+fields)
+        let detailResults = await Axios.get("https://api.artic.edu/api/v1/artworks/" + id + fields)
         // console.log('detailResults: ', detailResults);
-        this.setState({details: detailResults.data.data});
+        this.setState({ details: detailResults.data.data });
         // console.log('details', this.state.details);
-        this.setState({thumbnail: detailResults.data.data.thumbnail});
+        this.setState({ thumbnail: detailResults.data.data.thumbnail });
         // console.log('thumbnail', this.state.thumbnail)
-        this.setState({imageID: detailResults.data.data.image_id})
+        this.setState({ imageID: detailResults.data.data.image_id })
         this.apiGetImage();
-        }
+    }
 
     apiGetImage = async () => {
-        let imageResults = await Axios.get("https://www.artic.edu/iiif/2/"+this.state.imageID+"/full/600,/0/default.jpg");
-        console.log('imageResults: ',imageResults.config.url)
-        this.setState({imageResults: imageResults.config.url})
+        let imageResults = await Axios.get("https://www.artic.edu/iiif/2/" + this.state.imageID + "/full/600,/0/default.jpg");
+        console.log('imageResults: ', imageResults.config.url)
+        this.setState({ imageResults: imageResults.config.url })
     }
 
     render() {
         return (
             <div className="Details">
                 <div className="nav">
-                    
+
                     <Link to='/'>Home</Link>{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}
                     <Link to='/results'>Back to Results</Link>
+                </div>
+
+                {/* <div className='appName'>
+                    <p>
+                        <span className='b'>S</span>
+                        <span className='a'>A</span>
+                        <span className='s'>S</span>
+                        <span className='name'>Art Search</span>
+                    </p>
+
+                </div> */}
+                <div className="container">
+                    <div className="cube">
+                        <div className="face bottom">B</div>
+                        <div className="face left">B</div>
+                        <div className="face front">B</div>
+                    </div>
                 </div>
                 <div className="details">
                     <p><span className='title'>{this.state.details.title}</span> by {this.state.details.artist_title}</p>
@@ -53,7 +70,7 @@ class Details extends Component {
                     <p id='art-desc'>{this.state.thumbnail.alt_text}</p>
                 </div>
                 <div className="image">
-                    <img src={this.state.imageResults} alt_text=''/>
+                    <img src={this.state.imageResults} alt_text='' />
                 </div>
             </div>
         );
