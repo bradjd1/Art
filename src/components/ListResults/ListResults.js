199bbdc536659b5b162aca822ad1e44c23e7b109
Houtakker Brad L <hx37112@wdxc02z70awlvcg.jdnet.deere.com>
2021-09-10 12:59:12 -0500

    add logo to ListResults page

--- src/components/ListResults/ListResults.js
+++ src/components/ListResults/ListResults.js
@@ -27,6 +27,31 @@ class ListResults extends Component {
                 <div className='screen'>
                     <div className='background'>
                         <nav className='nav'>
+
+                            <span>
+                                <div className="container b-container">
+                                    <div className="cube">
+                                        <div className="face bottom b-box">B</div>
+                                        <div className="face left b-box">B</div>
+                                        <div className="face front b-box">B</div>
+                                    </div>
+                                </div>
+                                <div className="container a-container">
+                                    <div className="cube">
+                                        <div className="face bottom a-box">A</div>
+                                        <div className="face left a-box">A</div>
+                                        <div className="face front a-box">A</div>
+                                    </div>
+                                </div>
+                                <div className="container s-container">
+                                    <div className="cube">
+                                        <div className="face bottom s-box">S</div>
+                                        <div className="face left s-box">S</div>
+                                        <div className="face front s-box">S</div>
+                                    </div>
+                                </div>
+                            </span>
+                            <br></br><br></br>
                             <Link to='/'>Home</Link>
                         </nav>
                         <img src='../ArtInstituteofChicago2.jpg' alt='art pic' />
